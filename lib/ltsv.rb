50d6ad54c9d72d6e43ad8002674fc598c68c23d5
Tw. Shim <marocchino@gmail.com>
2013-03-16 15:34:41 +0900

    work in progress

--- lib/ltsv.rb
+++ lib/ltsv.rb
@@ -1,2 +1,14 @@
 class Ltsv
+  def initialize
+    @hash = {}
+  end
+
+  def set(key, value)
+    @hash[key] = value
+    nil
+  end
+
+  def get(key)
+    @hash[key]
+  end
 end
--- spec/ltsv_spec.rb
+++ spec/ltsv_spec.rb
@@ -3,5 +3,29 @@ require 'ltsv'
 
 
 describe Ltsv do
+  before :each do
+    @ltsv = Ltsv.new
+  end
 
+  context ".set" do
+    it "when key foo assign hoge should return nil" do
+      @ltsv.set("foo", "hoge").should be_nil
+    end
+
+    it "when key foo assign hoge should return nil" do
+      @ltsv.set("foo", "blabla").should be_nil
+    end
+  end
+
+  context ".get" do
+    it "when key foo assign hoge should return nil" do
+      @ltsv.set("foo", "hoge")
+      @ltsv.get("foo").should == "hoge"
+    end
+
+    it "when key foo assign hoge should return nil" do
+      @ltsv.set("foo", "blabla")
+      @ltsv.get("foo").should == "blabla"
+    end
+  end
 end
